a04814e34217eea5a1662d8b45c854afa9ad36da
vas g <vas.g@me.com>
2023-11-23 23:23:56 +0400

    add errors handler

--- core/resources/errors_handlers.py
+++ core/resources/errors_handlers.py
@@ -2,7 +2,7 @@ import typing
 
 import aiogram
 
-from core.config import Settings
+from core.config import settings
 
 if typing.TYPE_CHECKING:
     from aiogram.types.error_event import ErrorEvent
@@ -14,4 +14,4 @@ router = Router()
 
 @router.errors()
 async def error_handler(exception: 'ErrorEvent', bot: aiogram.Bot):
-    await bot.send_message(Settings.ADMIN_ID, 'shit happen')
+    await bot.send_message(settings.ADMIN_ID, 'shit happen')
